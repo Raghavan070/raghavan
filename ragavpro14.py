14f737c38a7d065e5a4f150b2b36e5745aa6fd22
Raghavan070 <53746416+Raghavan070@users.noreply.github.com>
2019-08-14 12:13:30 +0530

    Create ragavpro14.py

---
+++ ragavpro14.py
@@ -0,0 +1,14 @@
+#S
+vjs,vk=map(int,input().split())
+ls=list(map(int,input().split()))
+vjs=[]
+ls.insert(0,0)
+for a in range(vk):
+     v=[]
+     sumup=0
+     cc,dd=map(int,input().split())
+     for b in range(cc,dd+1):         
+         sumup^=ls[b]     
+     vjs.append(sumup)
+for c in vjs:
+    print(c)
