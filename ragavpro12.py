85c0783077c916f731ce3f975184fbff1d0f0487
Raghavan070 <53746416+Raghavan070@users.noreply.github.com>
2019-08-14 12:08:21 +0530

    Create ragavpro12.py

---
+++ ragavpro12.py
@@ -0,0 +1,12 @@
+#S
+a1,b1=map(int,input().split())
+lis=list(map(int,input().split()))
+l1=[]
+for i in range(0,b1):
+     u1,v1=map(int,input().split())
+     l1.append([u1,v1])
+for i in range(b1):
+     lower=l1[i][0]
+     upper=l1[i][1]
+     s=sum(lis[lower-1:upper])
+     print(s)
